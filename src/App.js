330b6a3c8b5edcf8febdf92e8e126a967dc6fcd6
unknown <omarmohammed271@gmail.com>
2024-12-23 11:37:36 +0200

    Solve third Challenge

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Post from './posts';
 import SideMenu from './sideBar';
 
 function App() {
+  const posts = [
+    {id:1, title: "Learn Python in 30 Day", content: "as you like you wanna reach" },
+    {id:2, title: "Learn React with Omar", content: "React is an important frame work" },
+    {id:3, title: "Learn JS" }
+  ];
+  const postsValue = posts.map((post) => (
+    <Post key={post.id} title={post.title} content={post.content} />
+  ));
+  console.log(posts);
   return (
 
     <div >
@@ -14,9 +23,11 @@ function App() {
       </div>
       <div className='container mx-auto mt-10 w-3/4 flex'>
         <div className='w-3/5 flex-auto'>
-          <Post title="Learn Python in 30 Day" content="as you like you wanna reach"/>
+        {postsValue}
+          {/* {posts.map((post) => <Post key={post.id} title={post.title} content={post.content} />)} */}
+          {/* <Post title="Learn Python in 30 Day" content="as you like you wanna reach"/>
           <Post title="Learn React with Omar" content="React is an important frame work" />
-          <Post title="Learn JS" />
+          <Post title="Learn JS" /> */}
           
         </div>
         <div className='flex-auto mx-8 w-1/5'>
--- src/sideBar.jsx
+++ src/sideBar.jsx
@@ -1,14 +1,27 @@
 import Button from "./buttons"
-export default function SideMenu(){
-    
-    return(
+
+export default function SideMenu() {
+    const buttons = [
+        {
+            id: 1,
+            children: (
+                <>
+                    <h1>Hello</h1>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s" alt="images" />
+                </>
+            )
+        },
+        {
+            id: 2,
+            children: <h1>Hi</h1>
+        }
+    ];
+
+    return (
         <div className="w-48 justify-center border-4 border-cyan-400 p-4 text-center mt-4 rounded-lg">
-            <Button><h1>Hello</h1>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s" alt="images" />
-            </Button>
-            <Button><h1>Hi</h1></Button>
-            <Button/>
-            
+            {buttons.map((button) => (
+                <Button key={button.id}>{button.children}</Button>
+            ))}
         </div>
-    )
+    );
 }
